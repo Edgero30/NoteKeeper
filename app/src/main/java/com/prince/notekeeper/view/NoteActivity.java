f17db4191cea6731f58ebfdfe793906a2e59b955
Prince Odia <princeodia@gmail.com>
2022-10-04 03:09:49 +0100

    Remove comments

--- app/src/main/java/com/prince/notekeeper/view/NoteActivity.java
+++ app/src/main/java/com/prince/notekeeper/view/NoteActivity.java
@@ -139,21 +139,16 @@ public class NoteActivity extends AppCompatActivity {
     private void createNewNote() {
         DataManager dm = DataManager.getInstance();
         mNotePosition = dm.createNewNote();
-//        mNote = dm.getNotes().get(mNotePosition);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_note, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         if (id == R.id.action_send_mail) {
